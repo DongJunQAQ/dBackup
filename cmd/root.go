c1c8b2656759e355abf2499ecb05426051d18e9f
DongJun <you@example.com>
2026-01-04 17:29:12 +0800

    feat: 初始化项目

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:     "dBackup",
+	Short:   "基于华为云的备份工具",
+	Long:    `一个基于华为云的备份工具，通过命令行的方式备份虚拟机、云硬盘、快照等`,
+	Version: "v0.1.0",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "dBackup/cmd"
+
+func main() {
+	cmd.Execute()
+}
