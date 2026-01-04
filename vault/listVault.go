1cf38e4d4975fa8c512676ef78252ee8d6584d64
DongJun <you@example.com>
2026-01-04 20:39:23 +0800

    fix: 美化vault list命令的输出

--- vault/listVault.go
+++ vault/listVault.go
@@ -29,7 +29,18 @@ func ListVault() { //列出备份存储库
 	request := &model.ListVaultRequest{}
 	response, err := client.ListVault(request)
 	if err == nil {
-		fmt.Printf("%+v\n", *response.Vaults)
+		fmt.Printf("%-36s | %-15s | %-5s | %-s\n", "ID", "Name", "Size", "Resources")
+		fmt.Println("--------------------------------------------------------------------------------")
+		// 检查 Vaults 是否为空
+		if response.Vaults != nil {
+			for _, v := range *response.Vaults {
+				size := 0
+				if v.Billing != nil {
+					size = int(v.Billing.Size)
+				}
+				fmt.Printf("%-36s | %-15s | %-5d | %v\n", v.Id, v.Name, size, v.Resources)
+			}
+		}
 	} else {
 		fmt.Println(err)
 	}
