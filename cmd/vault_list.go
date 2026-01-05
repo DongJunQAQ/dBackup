f2d1134e0d7e33c23fc2d56bc6aadefbc79c0b4d
DongJun <you@example.com>
2026-01-05 18:25:28 +0800

    feat: 设置子命令list的简短别名

--- cmd/vault_list.go
+++ cmd/vault_list.go
@@ -6,9 +6,9 @@ import (
 )
 
 var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "列出所有备份存储库",
-	Long:  `列出所有备份存储库`,
+	Use:     "list",
+	Aliases: []string{"l"}, //设置子命令的简短别名
+	Short:   "列出所有备份存储库",
 	Run: func(cmd *cobra.Command, args []string) {
 		vault.ListVault()
 	},
