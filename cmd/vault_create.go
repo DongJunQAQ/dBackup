877f73c9a8cb47d932b9b389dbb1cb7a11d638a6
DongJun <you@example.com>
2026-01-05 21:12:57 +0800

    feat: 添加vault list的flag

--- cmd/vault_create.go
+++ cmd/vault_create.go
@@ -8,7 +8,6 @@ import (
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "创建备份存储库",
-	Long:  `创建备份存储库`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called")
 	},
@@ -16,5 +15,15 @@ var createCmd = &cobra.Command{
 
 func init() {
 	vaultCmd.AddCommand(createCmd)
-
+	var vaultName string
+	var vaultSize int32
+	//添加本地flag
+	createCmd.Flags().StringVarP(&vaultName, "name", "n", "", "存储库名称")
+	createCmd.Flags().Int32VarP(&vaultSize, "size", "s", 0, "存储库大小")
+	//设置必选flag
+	err := createCmd.MarkFlagRequired("name")
+	err = createCmd.MarkFlagRequired("size")
+	if err != nil {
+		return
+	}
 }
