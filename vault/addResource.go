04c30c4815691a01c82ec052eabfbeaefa23a0aa
DongJun <you@example.com>
2026-01-06 22:06:24 +0800

    feat: 添加ID格式错误的报错信息

--- vault/addResource.go
+++ vault/addResource.go
@@ -10,6 +10,7 @@ import (
 var (
 	ErrUnknownType = errors.New("未知类型")
 	ErrNotExists   = errors.New("存储库或资源不存在")
+	ErrFormat      = errors.New("ID格式有误")
 )
 
 func AddResources(vaultId string, resourcesID string, resourcesType string) error { //往存储库中添加资源
@@ -43,7 +44,10 @@ func AddResources(vaultId string, resourcesID string, resourcesType string) erro
 	} else {
 		var serviceErr *sdkerr.ServiceResponseError //华为云SDK专门定义的错误结构体，里面包含了StatusCode、RequestId等详细信息
 		if errors.As(err, &serviceErr) {            //会检查err是不是属于*sdkerr.ServiceResponseError这种类型,如果是这种类型的错误errors.As会返回true，并自动把err里面的具体内容填充到serviceErr变量里
-			if serviceErr.StatusCode == 404 {
+			if serviceErr.ErrorCode == "BackupService.9900" {
+				return fmt.Errorf("%w", ErrFormat) //返回ID格式不对的错误
+			}
+			if serviceErr.ErrorCode == "BackupService.6302" || serviceErr.ErrorCode == "BackupService.6105" {
 				return fmt.Errorf("%w", ErrNotExists) //返回资源或存储库不存在的错误
 			}
 		}
