33b792f6c227e1b7f9ce96c6a3f977c6b3827836
DongJun <you@example.com>
2026-01-23 17:21:18 +0800

    feat: 为vault子命令添加别名

--- cmd/vault.go
+++ cmd/vault.go
@@ -5,9 +5,10 @@ import (
 )
 
 var vaultCmd = &cobra.Command{
-	Use:   "vault",
-	Short: "备份存储库相关操作",
-	Long:  `备份存储库相关操作，如创建、列出、添加资源等`,
+	Use:     "vault",
+	Aliases: []string{"v"},
+	Short:   "备份存储库相关操作",
+	Long:    `备份存储库相关操作，如创建、列出、添加资源等`,
 }
 
 func init() {
