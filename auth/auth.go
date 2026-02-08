3bfb00ba0e6d29ac973ec71e948a552fd4d713c1
DongJun <you@example.com>
2026-02-08 21:33:22 +0800

    chore: 提取出获取配置文件路径的函数

--- auth/auth.go
+++ auth/auth.go
@@ -17,18 +17,25 @@ type Cert struct { //凭证信息结构体
 var currentSecret = genRandSecret()                              //动态生成[]byte类型的密钥
 var secretStr = base64.StdEncoding.EncodeToString(currentSecret) //将密钥转换为String类型
 
+func getConfPath() (string, error) { //获取dBackup配置文件的完整路径
+	homeDir, err := os.UserHomeDir() //获取家目录
+	if err != nil {
+		return "", fmt.Errorf("无法获取家目录: %v\n", err)
+
+	}
+	filePath := filepath.Join(homeDir, ".dbackup_config.json") //拼接完整路径
+	return filePath, nil
+}
+
 func SaveAkSk(ak string, sk string) { //保存ak和sk至本地文件
 	conf := Cert{
 		AK: ak,
 		SK: sk,
 	}
-	//1.构建文件路径
-	homeDir, err := os.UserHomeDir() //获取家目录
+	filePath, err := getConfPath()
 	if err != nil {
-		fmt.Println("无法获取家目录:", err)
-		return
+		fmt.Println("无法获取配置文件的路径:", err)
 	}
-	filePath := filepath.Join(homeDir, ".dbackup_config.json") //构建完整路径
 	//2.序列化ak/sk结构体
 	content, _ := sonic.Marshal(conf) //将结构体序列化为[]byte类型
 	//3.加密ak/sk结构体
@@ -53,8 +60,10 @@ func SaveAkSk(ak string, sk string) { //保存ak和sk至本地文件
 
 func LoadAkSk() (string, string, error) { //从本地文件中读取加密后的ak/sk并解密
 	// 1. 获取家目录并构建路径
-	homeDir, _ := os.UserHomeDir()
-	filePath := filepath.Join(homeDir, ".dbackup_config.json")
+	filePath, err := getConfPath()
+	if err != nil {
+		return "", "", fmt.Errorf("无法获取配置文件的路径: %v", err)
+	}
 	// 2. 读取文件内容
 	fileData, err := os.ReadFile(filePath)
 	if err != nil {
