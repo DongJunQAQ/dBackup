d133abcc6324e6a5390e1863bb5252efa5c649e9
DongJun <you@example.com>
2026-02-21 15:38:20 +0800

    feat: 修改查看资源的命令方式

--- cmd/resource.go
+++ cmd/resource.go
@@ -12,10 +12,13 @@ var resourceCmd = &cobra.Command{
 	Short:   "查看相关资源",
 	Long:    `查看华为云上的各类资源如ECS、云硬盘等`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ecs, _ := cmd.Flags().GetBool("ecs")
-		if ecs {
+		resType, _ := cmd.Flags().GetString("type")
+		switch resType {
+		case "ecs":
 			resource.ListEcs()
-		} else { //如果用户没有输入flag则提醒用户并打印帮助信息
+		case "evs":
+			resource.ListEvs()
+		default:
 			pterm.Error.Println("无效命令")
 			_ = cmd.Help()
 		}
@@ -24,6 +27,6 @@ var resourceCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(resourceCmd)
-	var ecs bool
-	resourceCmd.Flags().BoolVarP(&ecs, "ecs", "e", false, "列出ECS服务器")
+	var resType string
+	resourceCmd.Flags().StringVarP(&resType, "type", "t", "ecs", "要查询的资源类型")
 }
