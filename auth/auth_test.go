6630d3f5bad07cd19b782bc2c68d3febf161540d
DongJun <you@example.com>
2026-02-23 17:27:36 +0800

    test: 修改auth模块的测试用例

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -1,7 +1,9 @@
 package auth
 
 import (
+	"encoding/base64"
 	"fmt"
+	"github.com/bytedance/sonic"
 	"testing"
 )
 
@@ -10,22 +12,23 @@ var conf = Cert{
 	SK: "Zz29pXObYTl4ynzcipz3ECdgmcljZAFKx5GOetzC",
 }
 
-//func TestSecret(t *testing.T) {
-//	fmt.Println("随机生成的密钥([]byte类型):", genRandSecret())
-//	fmt.Println("随机生成的密钥(String类型):", base64.StdEncoding.EncodeToString(genRandSecret()))
-//}
-//
-//func TestEncrypt(t *testing.T) {
-//	if len(currentSecret) != 32 {
-//		t.Errorf("密钥长度不为32位")
-//	}
-//	data, _ := sonic.Marshal(conf)
-//	res, err := encrypt(data, currentSecret)
-//	if err != nil {
-//		t.Error(err)
-//	}
-//	fmt.Println(res)
-//}
+func TestGenSecret(t *testing.T) {
+	secret := genTrueRand(32)
+	if len(secret) != 32 {
+		t.Fatalf("secret长度应为32")
+	}
+	fmt.Println("随机生成的密钥([]byte类型):", secret)
+	fmt.Println("随机生成的密钥(String类型):", base64.StdEncoding.EncodeToString(genTrueRand(32)))
+}
+
+func TestEncrypt(t *testing.T) {
+	data, _ := sonic.Marshal(conf) //将go对象序列化为字节流格式
+	res, err := encrypt(data, genTrueRand(32))
+	if err != nil {
+		t.Error("加密失败: ", err)
+	}
+	fmt.Println("auth字段的值为: ", res)
+}
 
 func TestSaveAkSk(t *testing.T) {
 	SaveAkSk(conf.AK, conf.SK)
