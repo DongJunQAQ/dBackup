1dfb8753916c5ef6d1d8bc142e9ee43cd32904ef
DongJun <you@example.com>
2026-03-11 21:14:13 +0800

    docs: 编辑README并发布v0.2.0版本

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,7 @@ var rootCmd = &cobra.Command{
 	Use:     "dBackup",
 	Short:   "基于华为云的备份工具",
 	Long:    `一个基于华为云的备份工具，通过命令行的方式备份虚拟机、云硬盘、对象存储、快照等`,
-	Version: "v0.1.0",
+	Version: "v0.2.0",
 }
 
 func Execute() {
